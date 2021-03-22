4f45707884b9cc9cb8b2ec9f0250dcd40216848e
Liviu Cornea <liviumcornea@gmail.com>
2021-03-21 21:21:59 -0400

    No store freeze

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -2,7 +2,6 @@ import { ActionReducer, ActionReducerMap, MetaReducer } from "@ngrx/store";
 import { AppState } from "../states/appStates";
 import { userReducer } from "./userReducer";
 import * as fromRouter from '@ngrx/router-store';
-import {storeFreeze} from 'ngrx-store-freeze';
 import { environment } from '../../../environments/environment';
 
 export const reducersMap: ActionReducerMap<AppState> = {
@@ -21,5 +20,5 @@ export const reducersMap: ActionReducerMap<AppState> = {
   }
 
   export const metaReducers: MetaReducer<AppState>[] = !environment.production
-    ? [logger, storeFreeze]
+    ? [logger]
     : [];
